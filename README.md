b0e19e418a273806b1ba33468c2df8e8e5dcd84b
klarakucerova <klarakucerova09@gmail.com>
2018-06-07 12:04:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# WordPress Skeleton Theme
+
+
+## About:
+This theme has been developed with Underscores by Automattic as base: [http://underscores.me/](http://underscores.me/)
+
+
+## SASS setup:
+- Make sure you have installed Node.js and Browser-Sync* (* optional, if you wanna use it) on your computer globally
+- To be able to compile Sass, you need to install node_modules locally by running `npm install` inside following directory:  wp-content/themes/wp-skeleton/.. 
+
+This should get you up and running!
+
+
+### Possible Sass setup errors:
+After running `npm install` you get Error: Cannot find module ‘module-name’
+This most probably means that this particular module is missing from your dependencies (package.json). The best way to fix it would be installing it manually by running `npm install module-name` (check the module docs for install instructions). This would then add it to your dependencies in `package.json` file.
+
+
+## SASS compilation:
+- To compile your sass, run `gulp` in the following directory: wp-content/themes/wp-skeleton/..
+
+
+## SASS file structure:
+If you want to add new sass files/components etc. please add them in the following directory: `wp-content/themes/wp-skeleton/src/scss/..`
+Every new sass file (e.g. `_name-of-your-file.scss`) needs to be called inside our `wp-content/themes/wp-skeleton/src/scss/_core.scss` to be included in sass compilation.
+
+All theme variable are inside `wp-content/themes/wp-skeleton/src/scss/_config.scss` file.
---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')(),
+    pngquant = require('imagemin-pngquant'),
+    browserSync = require('browser-sync'),
+    stylelint = require("stylelint"),
+    scssParser = require('postcss-scss'),
+    cleanCSS = require('gulp-clean-css'),
+    beeper = require('beeper'),
+    color = require('ansi-colors');
+
+var PATHS = {
+    localhost: 'wp-skeleton.local/',
+    images: {
+        dest: 'images/',
+        dir: 'src/images/'
+    },
+    sass: {
+        src: 'src/scss/',
+        dest: 'css/'
+    },
+    js: {
+        src: 'src/js/',
+        dest: 'js/'
+    },
+    css: {
+        src: 'css/',
+        dest: '.'
+    }
+}
+
+var onError = function (error) {
+    beeper();
+    console.log(color.red(error));
+    this.emit('end');
+};
+
+gulp.task('default', ['sass', 'sasslint', 'browser-sync', 'minify-css'], function () {
+    gulp.watch(PATHS.sass.src + '**/*.scss', ['sass', 'sasslint']);
+    gulp.watch(PATHS.images.dir + '**/*', ['imagemin']);
+    gulp.watch(PATHS.css.src + '*.css', ['minify-css']);
+    gulp.watch(PATHS.localhost + '*.html').on('change', browserSync.reload);
+    // gulp.watch(PATHS.js.src + '**/*', ['js']);
+});
+
+gulp.task('sasslint', function() {
+    var processors = [
+        stylelint({
+        "rules": {
+            "indentation": 4
+        }})
+    ];
+
+    gulp.src([PATHS.sass.src + '**/*.scss'])
+        .pipe(plugins.postcss(processors, {syntax: scssParser}));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(PATHS.sass.src + '**/*.scss')
+        .pipe(plugins.plumber({
+            errorHandler: onError
+        }))
+        .pipe(plugins.changed('css'))
+        .pipe(plugins.sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(plugins.autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(PATHS.sass.dest))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('imagemin', function () {
+    return gulp.src(PATHS.images.dir + '**/*')
+        .pipe(plugins.changed(PATHS.images.dest))
+        .pipe(plugins.size())
+        .pipe(plugins.imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(PATHS.images.dest))
+        .pipe(plugins.size());
+});
+
+// gulp.task('js', function () {
+//     return gulp.src(PATHS.js.src + '**/*.js')
+//         .pipe(plugins.sourcemaps.init())
+//         .pipe(plugins.eslint({
+//             configFile: '.eslintrc'
+//         }))
+//         .pipe(plugins.eslint.format())
+//         .pipe(plugins.jscs())
+//         .pipe(plugins.babel())
+//         .pipe(plugins.sourcemaps.write('.'))
+//         .pipe(gulp.dest(PATHS.js.dest));
+// });
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        proxy: PATHS.localhost
+    });
+});
+
+gulp.task('minify-css', () => {
+    return gulp.src(PATHS.css.src + '*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest(PATHS.css.dest));
+});
